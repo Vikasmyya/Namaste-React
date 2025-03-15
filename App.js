a9d23b9d48b24934350354c224809f30b3fdddba
Vikas v <vikasmyya@gmail.com>
2025-03-15 21:28:45 +0530

    EP.1

---
+++ App.js
@@ -0,0 +1,19 @@
+//craeting an element
+//1st parameter is what is the element
+//2nd is objects or like id
+//3rd is what we want to show in the element
+const  heading = React.createElement("h1",{
+    id:"heading"
+},"Hello world from React!")
+
+//element created by react is object
+//render method will help to  convert it into the html which browser will understand
+console.log(heading)
+
+//creating the root
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+//render the heading elemnt in the root
+root.render(heading)
+
+
